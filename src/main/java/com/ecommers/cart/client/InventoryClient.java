06557baf629e143e2d9ae03461932324cbad6b8d
dunedains <fe.zapatao@duocuc.cl>
2026-05-20 00:15:27 -0400

    fix feign client property names

--- src/main/java/com/ecommers/cart/client/InventoryClient.java
+++ src/main/java/com/ecommers/cart/client/InventoryClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "inventory-service", url = "${feign.client.inventory-url}")
+@FeignClient(name = "inventory-service", url = "${feign.client.inventory_url}")
 public interface InventoryClient {
 
     @GetMapping("/api/inventory/{productId}")
